3591dce7769375bf4bf24b82a150783293346073
TTeaTT <43846563+TTeaTT@users.noreply.github.com>
2019-02-26 19:25:55 +0000

    Update gamescript.js

--- gamescript.js
+++ gamescript.js
@@ -9,18 +9,55 @@ var coin1;
 var coin1Got = false;
 var score = 0;
 
+// KEYBOARD CONTROLS
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+function keyDownHandler(event) {
+    if(event.keyCode == 39) {
+        rightPressed = true;
+        moveRight();
+    }
+    else if(event.keyCode == 37) {
+        leftPressed = true;
+        moveLeft();
+    }else if(event.keyCode == 38) {
+    	upPressed = true;
+    	moveUp();
+    }
+}
+function keyUpHandler(event) {
+    if(event.keyCode == 39) {
+        rightPressed = false;
+        stopMove();
+    }
+    else if(event.keyCode == 37) {
+        leftPressed = false;
+        stopMove();
+    }
+    else if(event.keyCode == 38) {
+    	upPressed = false;
+    	stopMove();
+    }
+}
+
+
 // FUNCTION TO START THE GAME
 function startGame() {
 	gameArea.start();
 
-	floor1 = new component (100, 50, "black", 200,200)
-	danger1 = new component(20, 50, "red", 200, window.innerHeight*0.75-50);
-	danger2 = new component(20, 80, "red", 400, window.innerHeight*0.75-80);
+	floor1 = new component(100, 50, "black", 200, 200)
+	danger1 = new component(20, 50, "red", 200, window.innerHeight * 0.75 - 50);
+	danger2 = new component(20, 80, "red", 400, window.innerHeight * 0.75 - 80);
 	key = new component(65, 41, "img/small_key.png", 200, 250, "image");
-	door = new component(100, 150, "img/door.png", window.innerWidth - 150, window.innerHeight*0.75-150,
-			"image");
+	door = new component(100, 150, "img/door.png", window.innerWidth - 150,
+			window.innerHeight * 0.75 - 150, "image");
 	coin1 = new component(30, 30, "img/coin.png", 500, 100, "image");
-	player = new component(50, 50, "img/player.png", 10, (window.innerHeight*0.75-50), "image");
+	player = new component(50, 50, "img/player.png", 10,
+			(window.innerHeight * 0.75 - 50), "image");
 }
 
 // CREATE A COMPONENT
@@ -36,14 +73,14 @@ function component(width, height, color, x, y, type) {
 	this.speedY = 0;
 	this.x = x;
 	this.y = y;
-//	this.gravity += 0.05;
-//	// this.gravitySpeed = 0;
+	// this.gravity += 0.05;
+	// // this.gravitySpeed = 0;
 	this.gravity = 0.05;
 	this.gravitySpeed = 0;
 	this.update = function() {
 		ctx = gameArea.context;
 		ctx.font = "40px Verdana";
-		ctx.fillText(("Score: "+score), 10, 50);
+		ctx.fillText(("Score: " + score), 10, 50);
 		ctx.fillText(("Time: "), 10, 90);
 		ctx.fillStyle = "white";
 		if (type == "image") {
@@ -60,20 +97,21 @@ function component(width, height, color, x, y, type) {
 		this.hitBottom();
 		this.hitLeft();
 		this.hitRight();
+		
 	}// end this.newPos
-	
+
 	this.hitBottom = function() {
 		if (player.y > gameArea.canvas.height - player.height) {
 			player.y = (gameArea.canvas.height - player.height);
 		}
 	}
-//	this.hitTop = function() {
-//	if (player.y < player.height) {
-//		player.y = player.height);
-//	}
-//}
+// this.hitTop = function() {
+// if (player.y < player.height) {
+// player.y = player.height);
+// }
+// }
 	this.hitLeft = function() {
-		if (player.x < 0 ) {
+		if (player.x < 0) {
 			player.x = 0;
 		}
 	}
@@ -107,7 +145,7 @@ var gameArea = {
 	canvas : document.createElement("canvas"),
 	start : function() {
 		this.canvas.width = window.innerWidth;
-		this.canvas.height = window.innerHeight*0.75;
+		this.canvas.height = window.innerHeight * 0.75;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, 20);
@@ -134,15 +172,15 @@ var gameArea = {
 
 // MOVING
 function moveUp() {
-	player.speedY -= 20;
+	player.speedY = -20;
 	player.gravity = -0.2;
 }
 
 function moveLeft() {
-	player.speedX -= 5;
+	player.speedX = -5;
 }
 function moveRight() {
-	player.speedX += 5;
+	player.speedX = 5;
 }
 function stopMove() {
 	player.speedX = 0;
@@ -165,22 +203,34 @@ function updateGameArea() {
 	if (player.crashWith(key)) {
 		document.getElementById("keyslot").src = "img/key.png";
 		keyGot = true;
-		key.y = -500; //not an elegant way to make it disappear, but works for now...
+		key.y = -500; // not an elegant way to make it disappear, but works
+						// for now...
 	}
-	
-	if (player.crashWith(door) && keyGot){
+
+	if (player.crashWith(door) && keyGot) {
 		window.location.href = "scores.html";
 	}
-	
-	if (player.crashWith(coin1)){
+
+	if (player.crashWith(coin1)) {
 		score = score + 100;
 		coin1Got = true;
-		coin1.y=-500;//not an elegant way to make it disappear, but works for now...
+		coin1.y = -500;// not an elegant way to make it disappear, but works
+						// for now...
 	}
-	 if (player.crashWith(floor1)) {
-	 player.y = floor1.y-floor1.height;
-	 player.speedY = 0;
-	 }
+	if (player.crashWith(floor1)) {
+		player.y = floor1.y - floor1.height;
+		player.speedY = 0;
+		
+// if (mybottom == othertop){
+// player.y = othertop -1;
+// } else if (mytop == otherbottom){
+// player.y = otherbottom + player.height +1;
+// } else if (myright == otherleft){
+// player.x = otherleft - player.width -1;
+// } else if (myleft == otherright) {
+// player.x = otherright +1;
+// }
+	}
 	gameArea.clear();
 	floor1.update();
 	danger1.update();
@@ -190,13 +240,5 @@ function updateGameArea() {
 	coin1.update();
 	player.newPos();
 	player.update();
-	score.uodate();
+	
 }
-
-
-
-
-
-
-
-
