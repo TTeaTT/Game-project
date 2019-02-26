a4278a7b6d599b7653f316b2ade769ea829ccb6a
TTeaTT <43846563+TTeaTT@users.noreply.github.com>
2019-02-26 14:56:17 +0000

    Updates
    26.2.

--- gamescript.js
+++ gamescript.js
@@ -6,20 +6,21 @@ var key;
 var door;
 var keyGot = false;
 var coin1;
+var coin1Got = false;
+var score = 0;
 
 // FUNCTION TO START THE GAME
 function startGame() {
 	gameArea.start();
 
-	floor1 = new component(window.innerWidth / 2, window.innerHeight, "black",
-			0, 350)
-	danger1 = new component(20, 50, "red", 200, 300);
-	danger2 = new component(20, 80, "red", 400, 270);
+	floor1 = new component (100, 50, "black", 200,200)
+	danger1 = new component(20, 50, "red", 200, window.innerHeight*0.75-50);
+	danger2 = new component(20, 80, "red", 400, window.innerHeight*0.75-80);
 	key = new component(65, 41, "img/small_key.png", 200, 250, "image");
-	door = new component(100, 150, "img/door.png", window.innerWidth - 150, 50,
+	door = new component(100, 150, "img/door.png", window.innerWidth - 150, window.innerHeight*0.75-150,
 			"image");
 	coin1 = new component(30, 30, "img/coin.png", 500, 100, "image");
-	player = new component(50, 50, "img/player.png", 10, 300, "image");
+	player = new component(50, 50, "img/player.png", 10, (window.innerHeight*0.75-50), "image");
 }
 
 // CREATE A COMPONENT
@@ -35,10 +36,16 @@ function component(width, height, color, x, y, type) {
 	this.speedY = 0;
 	this.x = x;
 	this.y = y;
-	// this.gravity = 0.05;
-	// this.gravitySpeed = 0;
+//	this.gravity += 0.05;
+//	// this.gravitySpeed = 0;
+	this.gravity = 0.05;
+	this.gravitySpeed = 0;
 	this.update = function() {
 		ctx = gameArea.context;
+		ctx.font = "40px Verdana";
+		ctx.fillText(("Score: "+score), 10, 50);
+		ctx.fillText(("Time: "), 10, 90);
+		ctx.fillStyle = "white";
 		if (type == "image") {
 			ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
 		} else {
@@ -47,10 +54,34 @@ function component(width, height, color, x, y, type) {
 		}// end else
 	}// end this.update
 	this.newPos = function() {
-		// this.gravitySpeed += this.gravity;
+		this.gravitySpeed += this.gravity;
 		this.x += this.speedX;
-		this.y += this.speedY;// + this.gravitySpeed;
+		this.y += this.speedY + this.gravitySpeed;
+		this.hitBottom();
+		this.hitLeft();
+		this.hitRight();
 	}// end this.newPos
+	
+	this.hitBottom = function() {
+		if (player.y > gameArea.canvas.height - player.height) {
+			player.y = (gameArea.canvas.height - player.height);
+		}
+	}
+//	this.hitTop = function() {
+//	if (player.y < player.height) {
+//		player.y = player.height);
+//	}
+//}
+	this.hitLeft = function() {
+		if (player.x < 0 ) {
+			player.x = 0;
+		}
+	}
+	this.hitRight = function() {
+		if (player.x > gameArea.canvas.width - player.width) {
+			player.x = (gameArea.canvas.width - player.width);
+		}
+	}
 
 	// COLLISION
 	this.crashWith = function(otherobj) {
@@ -76,7 +107,7 @@ var gameArea = {
 	canvas : document.createElement("canvas"),
 	start : function() {
 		this.canvas.width = window.innerWidth;
-		this.canvas.height = window.innerHeight;
+		this.canvas.height = window.innerHeight*0.75;
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 		this.interval = setInterval(updateGameArea, 20);
@@ -103,7 +134,8 @@ var gameArea = {
 
 // MOVING
 function moveUp() {
-	player.speedY -= 5;
+	player.speedY -= 20;
+	player.gravity = -0.2;
 }
 
 function moveLeft() {
@@ -115,6 +147,7 @@ function moveRight() {
 function stopMove() {
 	player.speedX = 0;
 	player.speedY = 0;
+	player.gravity = 0.05;
 }
 
 // UPDATE GAME AREA
@@ -124,6 +157,9 @@ function updateGameArea() {
 		gameArea.stop();
 		if (keyGot == true) {
 			key.y = -500;
+		}
+		if (coin1Got == true) {
+			coin1.y = -500;
 		}
 	}
 	if (player.crashWith(key)) {
@@ -137,12 +173,14 @@ function updateGameArea() {
 	}
 	
 	if (player.crashWith(coin1)){
+		score = score + 100;
+		coin1Got = true;
 		coin1.y=-500;//not an elegant way to make it disappear, but works for now...
 	}
-	// if (player.crashWith(floor1)) {
-	// player.y = 250;
-	// player.speedY = 0;
-	// }
+	 if (player.crashWith(floor1)) {
+	 player.y = floor1.y-floor1.height;
+	 player.speedY = 0;
+	 }
 	gameArea.clear();
 	floor1.update();
 	danger1.update();
@@ -152,5 +190,13 @@ function updateGameArea() {
 	coin1.update();
 	player.newPos();
 	player.update();
-	
+	score.uodate();
 }
+
+
+
+
+
+
+
+
